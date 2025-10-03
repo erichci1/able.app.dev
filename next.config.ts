ee1b453e9eb8f623ed5eb3e6d918a0af8ec32470
Eric <erichci@gmail.com>
2025-10-03 17:52:26 -0500

    changing next.config.ts for dev env

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,7 @@ const nextConfig: NextConfig = {
     output: "standalone",
     images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
     experimental: {
-        optimizeCss: false, // ⛔ disable LightningCSS optimizer
+        optimizeCss: false, // ⛔ Next: don't use LightningCSS
     },
 };
-
 export default nextConfig;
